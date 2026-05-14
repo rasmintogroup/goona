60706a73bdd68c31b48f339d2c3409aaa26926bf
rasmintogroup <rasmintogroup@gmail.com>
2026-05-14 21:55:28 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,62 +1,13 @@
 export default {
-  async fetch(request, env) {
+  async fetch(request) {
 
-    const corsHeaders = {
-      "Access-Control-Allow-Origin": "https://goona.my.id",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Access-Control-Allow-Headers": "*",
-      "Content-Type": "application/json"
-    };
+    return new Response("OK", {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "*"
+      }
+    });
 
-    // HANDLE PREFLIGHT
-    if (request.method === "OPTIONS") {
-      return new Response(null, {
-        status: 204,
-        headers: corsHeaders
-      });
-    }
-
-    try {
-
-      const bodyDariBrowser =
-      await request.json();
-
-      const response = await fetch(
-        "https://openrouter.ai/api/v1/chat/completions",
-        {
-          method: "POST",
-
-          headers: {
-            "Authorization":
-            `Bearer ${env.OPENROUTER_API_KEY}`,
-
-            "Content-Type":
-            "application/json"
-          },
-
-          body: JSON.stringify(bodyDariBrowser)
-        }
-      );
-
-      const data =
-      await response.text();
-
-      return new Response(data, {
-        status: response.status,
-        headers: corsHeaders
-      });
-
-    } catch (error) {
-
-      return new Response(
-        JSON.stringify({
-          error: error.message
-        }),
-        {
-          status: 500,
-          headers: corsHeaders
-        }
-      );
-    }
   }
-};
+}
