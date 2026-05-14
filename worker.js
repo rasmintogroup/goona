ec62b03f716686e95014518f117e532190e11023
rasmintogroup <rasmintogroup@gmail.com>
2026-05-14 21:59:52 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,17 @@
 export default {
   async fetch(request) {
 
+    // WAJIB ADA
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+          "Access-Control-Allow-Headers": "*"
+        }
+      });
+    }
+
     return new Response("OK", {
       headers: {
         "Access-Control-Allow-Origin": "*",
