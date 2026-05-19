52ad725b7e774d47a85cb3a270173016797d1c7a
rasmintogroup <rasmintogroup@gmail.com>
2026-05-19 07:27:20 +0700

    Create pertanyaan_daerah.js

---
+++ kosakata/pertanyaan_daerah.js
@@ -0,0 +1,7 @@
+export const daerahQuestions = [
+    "Wah keren! Kalau asal daerah atau domisili Kak [Nama] saat ini di kota/kabupaten mana? 🌍",
+    "Sip deh Kak [Nama]! Oh ya, sekarang posisi Kakak lagi tinggal di kota atau daerah mana nih di Kepri? 🏝️",
+    "Salam kenal Kak [Nama]! Biar Goona bisa cariin info yang paling dekat, domisili Kakak di daerah mana ya? 📍",
+    "Noted Kak [Nama]! Sekarang Goona mau tahu dong, Kakak lagi chat dari kota atau kabupaten mana? 😊",
+    "Nama yang bagus, Kak [Nama]! Nah, kalau boleh tahu, daerah asal atau tempat tinggal Kakak sekarang di mana? 🗺️"
+];
