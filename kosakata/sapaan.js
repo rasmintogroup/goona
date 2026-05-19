4701a14161a13db5dba6c74fd1b1bcd3dc0e7c8d
rasmintogroup <rasmintogroup@gmail.com>
2026-05-19 21:04:25 +0700

    Update sapaan.js

--- kosakata/sapaan.js
+++ kosakata/sapaan.js
@@ -213,6 +213,7 @@ export const modul1Variations = [
     "Halo! Selamat beraktivitas. Nama panggilan Kakak siapa?",
     "Hai! Terima kasih sudah bergabung. Siapa nama panggilan Kakak?",
     "Halo! Senang sekali. Nama panggilan Kakak siapa?"
+    ]
 ];
 
 export const modul2Variations = [
@@ -651,4 +652,5 @@ export const modul3Variations = [
     "Hai Kak [Nama]! Terima kasih sudah memberikan masukan.",
     "Halo Kak [Nama]! Semoga hari ini sangat berkesan buat Kakak.",
     "Hai Kak [Nama]! Sampai jumpa, semoga sukses terus!"
+    ]
 ];
